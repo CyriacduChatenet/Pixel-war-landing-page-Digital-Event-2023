c5229025a7bde7862c22ccb98907f5353891407a
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-11 16:13:12 +0100

    [ADD] Add authentication with firebase

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth"
+import firestoreDb from "../services/firebase.service";
 const Form = () => {
+
+  const [credentials, setCredentials] = useState({})
+
+  const [error, setError] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setCredentials({
+      ...credentials,
+      [name]: value
+    })
+    setError(false)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth, credentials.email,credentials.password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      updateProfile(user, {
+        displayName: credentials.username
+      })
+      .then(() => {
+        console.log("user created successfully");
+        setCredentials({})
+      })
+      .catch((error) => {
+        console.log(error.message)
+        setError(true)
+      })
+    })
+    .catch((error) => {
+      console.log(error.message)
+      setError(true)
+    });
+  }
+
   return (
-    <form className="formulaire" id="form" action="">
+    <form className="formulaire" id="form" onSubmit={handleSubmit}>
       <h2>
         Créée ton compte <br />
         et rejoins la bataille !
@@ -11,6 +53,7 @@ const Form = () => {
         name="email"
         id="name"
         placeholder="E-mail"
+        onInput={handleChange}
       />
       <input
         className="input-form"
@@ -18,6 +61,7 @@ const Form = () => {
         name="username"
         id="email"
         placeholder="Username"
+        onInput={handleChange}
       />
       <input
         className="input-form"
@@ -25,8 +69,12 @@ const Form = () => {
         name="password"
         id="phone"
         placeholder="Password"
+        minLength={6}
+        onInput={handleChange}
       />
       <button>S'inscrire</button>
+
+      {error ? <p>Une erreur s'est produite. Veuillez réesayer plus tard 😢</p> : null}
     </form>
   );
 };
